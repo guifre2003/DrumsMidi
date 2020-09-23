283dcc5191f3be138779ecd1f5fe5c742c7d8774
guifre2003 <guifresalvatpique@gmail.com>
2020-09-23 16:47:24 +0200

    modificacio readme, afegeixo programa

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # trLinux
+
+## IDE
+  PyCharm
+
+## Python Version
+  3.8.3150.1013
+
+
+## Llibreries
+- Python Mido (Detecció de notes)
+- Python-Rtmidi (gestió dels ports)
+
+## Installation (Ubuntu 18.04 LTS - Native & VirtualEnv)
+
+```
+pip3 list --format columns
+sudo apt-get install libasound2-dev (#include <alsa/asoundlib.h> error solution)
+pip3 install python-rtmidi --install-option="--no-jack"
+pip3 install mido
+  
+```
+
+## Execution 
+
+```
+python3 midi_receiver.py
+```
---
+++ midi_receiver.py
@@ -0,0 +1,32 @@
+import time
+import mido
+import sys
+
+# port_name = 'USB MIDI Interface 0'
+port_name = 'USB MIDI Interface:USB MIDI Interface MIDI 1 20:0'
+file1 = open('inputs.txt', 'w')
+
+# Comprobem que haguem escrit el nom del port be y que el USB estigui connectat.
+if not port_name in mido.get_input_names():
+    raise Exception("No es troba el port '{port_name}' a la llista de ports d'entrada disponibles: {ports}")
+
+# Aqui comenca realment el programa.
+tempo = int(input("What BPM do you want?"))
+milisecond_tempo = 60 / tempo * 1000
+input_port = mido.open_input(port_name)
+print(milisecond_tempo)
+start = time.time()
+timer = 0
+while timer < 250:
+    # El while acabarà als 10 segons.
+    for msg in input_port.iter_pending():
+        end = time.time()
+        msg.time = int(((end - start) * 1000) - ((end - start) * 1000) % (milisecond_tempo / 8))
+        # El mòdul serveix per quantitzar (ara esta a semicorxeres, una quarta part de negra).
+        file1.write(str(msg) + '\n')
+        print(msg)
+    time.sleep(0.02)
+    timer += 1
+file1.close()
+
+sys.exit()
