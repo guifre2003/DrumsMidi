bb22dcc38a38d5c91b0d06cae4418d8659c81523
Xavier Maltas Tarridas <xavi.maltass@gmail.com>
2020-10-28 17:22:41 +0100

    partiture maker file

---
+++ partitureMaker.py
@@ -0,0 +1,15 @@
+#import lilyPond
+# import imp
+
+# def getVarFromFile('inputs.txt')
+f = open('inputs.txt', 'r')
+# global data
+# imp.load_source('data', '', f)
+# f.close()
+lines = f.readlines()
+count = 0
+
+for line in lines:
+        print(line)
+    # Ara amb un if extreure el note_on, note, time.
+    # Una vegada tingui les 3 variables cridare lilypond amb les 3 variables.
