48307f84c5a4efe64a135598024b0d755ecb1bd2
Jan Vermeulen <jan.vermeulen@isencia.com>
2021-01-12 21:14:20 +0100

    Convert each line in a Dictionary with proper types
    Parse each line from the input and store lines with 'note_on' in a Dictionary with the proper type (int or float) for each value.
    All dictionaries are than added to a List called 'dictionaries'. That List can than be processed, and the values in the each Dictionary can be extracted.

--- partitureMaker.py
+++ partitureMaker.py
@@ -1,27 +1,42 @@
 from io import open
-import re
 
-txt = open("inputs.txt", "r")
-
-lines = {}
-line_num = 1
+txt = open("c:/temp/inputs.txt", "r")
+dictionaries = []
 while True:
     # read line
     line = txt.readline()
     if not line:
         break
-    matches = re.match('note_on', line)
-    if matches:
-        lines[line_num] = line
-        line_num += 1
-txt.close()
-print(lines)
+    
+    # discard lines with 'note_off'
+    if line.startswith('note_off'):
+        continue
+    
+    # create a dictionary for each line
+    dictionary = {}
+    dictionaries.append(dictionary) 
 
-total_lines_num = len(lines)
-lines_num = 1
-note_search = r'\w+note\w+'
-while lines_num <= total_lines_num:
-    notes = re.match(note_search, lines[lines_num])
-    if notes:
-        print(notes)
-    lines_num += 1
+    # trim 'note_on' and the trailing EOL from the line 
+    line = line[8:-1]
+    
+    # split into substrings by key-value
+    lineparts = line.split(' ')
+    
+    # extract the values from line and add them to the dictionary (as int or float)
+    for linepart in lineparts:
+        if linepart.startswith('channel'):
+            dictionary['channel'] = int(linepart[8:])
+        elif linepart.startswith('note'):
+            dictionary['note'] = int(linepart[5:])
+        elif linepart.startswith('velocity'):
+            dictionary['velocity'] = int(linepart[9:])
+        elif linepart.startswith('time'):
+            dictionary['time'] = float(linepart[5:])   
+        
+txt.close()
+
+for dictionary in dictionaries:
+    # print the dictionary
+    print(dictionary)
+    # print only the note
+    print('note -> '+str(dictionary['note']))
