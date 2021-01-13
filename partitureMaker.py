4900e2a3b1d0f10f3b19b9745a509861b1a20bcd
guifre2003 <guifresalvatpique@gmail.com>
2021-01-13 14:55:21 +0100

    Checkout of note_on deleted and error with int in line:37

--- partitureMaker.py
+++ partitureMaker.py
@@ -1,6 +1,7 @@
 from io import open
 
-#"c:/temp/inputs.txt"
+# instrument = input("Introdueix l'instrument ('bateria' o  'piano')")
+# "c:/temp/inputs.txt"
 txt = open("inputs.txt", "r")
 dictionaries = []
 while True:
@@ -10,7 +11,11 @@ while True:
         break
     
     # discard lines with 'note_off'
-    if line.startswith('note_off'):
+    # if line.startswith('note_off'):
+    #     continue
+
+    # discard lines with no information
+    if len(line) < 49:
         continue
     
     # create a dictionary for each line
@@ -18,22 +23,24 @@ while True:
     dictionaries.append(dictionary) 
 
     # trim 'note_on' and the trailing EOL from the line 
-    line = line[8:-1]
+    line = line[:-1]
     
     # split into substrings by key-value
     lineparts = line.split(' ')
     
     # extract the values from line and add them to the dictionary (as int or float)
     for linepart in lineparts:
-        if linepart.startswith('channel'):
-            dictionary['channel'] = int(linepart[8:])
-        elif linepart.startswith('note'):
-            dictionary['note'] = int(linepart[5:])
-        elif linepart.startswith('velocity'):
-            dictionary['velocity'] = int(linepart[9:])
-        elif linepart.startswith('time'):
-            dictionary['time'] = float(linepart[5:])   
-        
+        if line.startswith('note_on'):
+            if linepart.startswith('channel'):
+                dictionary['channel'] = int(linepart[8:])
+            elif linepart.startswith('note'):
+                dictionary['note'] = int(linepart[5:])
+            elif linepart.startswith('velocity'):
+                dictionary['velocity'] = int(linepart[9:])
+            elif linepart.startswith('time'):
+                dictionary['time'] = float(linepart[5:])
+        elif line.startswith('note_off'):
+            print(dictionaries.index)
 txt.close()
 
 for dictionary in dictionaries:
